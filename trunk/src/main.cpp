b5d0fbb045265cab5c22a39d9d398b0978273bf7
jaforthofer <jaforthofer@eea7cbae-53dd-4913-9fd5-15cdb2ef626d>
2014-08-29 16:20:21 +0000

    it's working

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -9,17 +9,17 @@ int main() {
 
     //Set inputs
     wind.C.leafAreaIndex = 1.0;
-    wind.C.h = 10.0;                        //canopy height (m)
+    wind.C.canopyHeight = 10.0;                        //canopy height (m)
     wind.C.z0g = 0.025;                      //ground roughness length (m)
     wind.C.dragCoefAth = 0.2;
     wind.C.heightMaxFoliageDist = 0.5;     //height of maximum foliage distribution for the normal distribution (m)
-    wind.C.standardDevFoliageDist = 0.25;   //standard deviation of foliage distribution for the normal distribution (m)
+    wind.C.standardDevFoliageDist = 0.05;   //standard deviation of foliage distribution for the normal distribution (m)
     wind.C.numNodes = 10001;                 //number of cells to use for numerical integration
 
     wind.C.initialize();
     wind.computeWind();
 
-    wind.plot();
+    wind.plotWind(15.0, wind.C.canopyHeight + 5.0);
 
     std::cout << "Done!" << std::endl;
     return 0;
