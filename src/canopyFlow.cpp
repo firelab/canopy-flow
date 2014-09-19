cbe4ada5f1d8edf7e43756c07f464dab054429e0
Jason Forthofer <jforthofer@users.noreply.github.com>
2014-09-19 22:22:39 +0000

    getting close to having triangle distribution implemented

--- src/canopyFlow.cpp
+++ src/canopyFlow.cpp
@@ -2,6 +2,7 @@
 
 canopyFlow::canopyFlow()
 {
+    C = NULL;
     usuh = 0.0;
     uzc = NULL;
     uzcs = NULL;
@@ -17,11 +18,25 @@ canopyFlow::canopyFlow()
 
 canopyFlow::canopyFlow(canopyFlow &rhs)
 {
+    if(rhs.C)
+        make_canopy(rhs.C);
+    else
+        C = NULL;
     usuh = rhs.usuh;
     if(rhs.uzc)
-        memcpy(uzc, rhs.uzc, sizeof(double) * C.numNodes);
+    {
+        uzc = new double[C->numNodes];
+        memcpy(uzc, rhs.uzc, sizeof(double) * C->numNodes);
+    }else{
+        uzc = NULL;
+    }
     if(rhs.uzcs)
-        memcpy(uzcs, rhs.uzcs, sizeof(double) * C.numNodes);
+    {
+        uzcs = new double[C->numNodes];
+        memcpy(uzcs, rhs.uzcs, sizeof(double) * C->numNodes);
+    }else{
+        uzcs = NULL;
+    }
     doh = rhs.doh;
     z0oh = rhs.z0oh;
     K = rhs.K;
@@ -36,11 +51,27 @@ canopyFlow &canopyFlow::operator=(const canopyFlow &rhs)
 {
     if(&rhs != this)
     {
+        if(rhs.C)
+            make_canopy(rhs.C);
+        else
+            C = NULL;
         usuh = rhs.usuh;
         if(rhs.uzc)
-            memcpy(uzc, rhs.uzc, sizeof(double) * C.numNodes);
+        {
+            delete[] uzc;
+            uzc = new double[C->numNodes];
+            memcpy(uzc, rhs.uzc, sizeof(double) * C->numNodes);
+        }else{
+            uzc = NULL;
+        }
         if(rhs.uzcs)
-            memcpy(uzcs, rhs.uzcs, sizeof(double) * C.numNodes);
+        {
+            delete[] uzcs;
+            uzcs = new double[C->numNodes];
+            memcpy(uzcs, rhs.uzcs, sizeof(double) * C->numNodes);
+        }else{
+            uzcs = NULL;
+        }
         doh = rhs.doh;
         z0oh = rhs.z0oh;
         K = rhs.K;
@@ -55,6 +86,8 @@ canopyFlow &canopyFlow::operator=(const canopyFlow &rhs)
 
 canopyFlow::~canopyFlow()
 {
+    delete C;
+    C = NULL;
     delete[] uzc;
     uzc = NULL;
     delete[] uzcs;
@@ -63,14 +96,14 @@ canopyFlow::~canopyFlow()
 
 void canopyFlow::plot()
 {
-    double* y = new double[C.numNodes];
-    for(int i=0; i<C.numNodes; i++)
+    double* y = new double[C->numNodes];
+    for(int i=0; i<C->numNodes; i++)
     {
-        y[i] = i*C.cellsize;
+        y[i] = i*C->cellsize;
     }
 
-    double* x = new double[C.numNodes];
-    for(int i=0; i<C.numNodes; i++)
+    double* x = new double[C->numNodes];
+    for(int i=0; i<C->numNodes; i++)
     {
         x[i] = uzcs[i];
     }
@@ -116,7 +149,7 @@ void canopyFlow::plot()
     //  - plot_symbol=9 sets a circle with a dot in the
     // middle for the plot symbol - see "man plpoin"
     //pls->poin( numNodes, (PLFLT*) x,(PLFLT*) y, 9 );
-    pls->line( C.numNodes, (PLFLT*) x,(PLFLT*) y);
+    pls->line( C->numNodes, (PLFLT*) x,(PLFLT*) y);
 
     delete pls; // close plot
 
@@ -139,17 +172,17 @@ void canopyFlow::plotWind(double inputSpeed, double inputHeight)
     for(int i=0; i<cells; i++)
         windSpeed[i] = get_windspeed(inputSpeed, inputHeight, i*cellsize);
 
-    double* zCanopy = new double[C.numNodes];   //set up canopy density z axis
-    for(int i=0; i<C.numNodes; i++)
-        zCanopy[i] = i * C.cellsize * C.canopyHeight;
+    double* zCanopy = new double[C->numNodes];   //set up canopy density z axis
+    for(int i=0; i<C->numNodes; i++)
+        zCanopy[i] = i * C->cellsize * C->canopyHeight;
 
     PLFLT xmin =0, ymin=0, xmax=windSpeed[cells-1], ymax=z[cells-1];
 
-    double maxHaz = C.haz[0];
-    for(int i=0; i<C.numNodes; i++)
+    double maxHaz = C->haz[0];
+    for(int i=0; i<C->numNodes; i++)
     {
-        if(C.haz[i]>maxHaz)
-            maxHaz = C.haz[i];
+        if(C->haz[i]>maxHaz)
+            maxHaz = C->haz[i];
     }
 
     double maxWind = windSpeed[0];
@@ -215,9 +248,9 @@ void canopyFlow::plotWind(double inputSpeed, double inputHeight)
     xFill[0] = 0;
     yFill[0] = 0;
     xFill[1] = 0;
-    yFill[1] = C.canopyHeight;
+    yFill[1] = C->canopyHeight;
     xFill[2] = maxWind;
-    yFill[2] = C.canopyHeight;
+    yFill[2] = C->canopyHeight;
     xFill[3] = maxWind;
     yFill[3] = 0;
     pls->scol0a( 0, 0, 255, 0, 0.5);
@@ -234,12 +267,12 @@ void canopyFlow::plotWind(double inputSpeed, double inputHeight)
     pls->schr(0, 1.0);  //change font size
     pls->mtex( "b", 3.0, 0.5, 0.5, "wind speed" );
     pls->mtex( "l", 3.0, 0.5, 0.5, "height" );
-    //pls->wind(0.0, max, 0.0, zCanopy[C.numNodes-1]);  //reset window to haz coordinates
+    //pls->wind(0.0, max, 0.0, zCanopy[C->numNodes-1]);  //reset window to haz coordinates
     plscol0(1, 14, 143, 14);    //first change the color pallet0 first color(#1) to be black (0,0,0)
     plcol0(1);              //now change our font color to be the color #1
     pls->wind(0.0, maxHaz, ymin, ymax);  //reset window to haz coordinates
     pls->box("cmstv", 0.0, 0, "", 0.0, 0);
-    pls->line(C.numNodes, (PLFLT*) C.haz,(PLFLT*) zCanopy);   //plot haz
+    pls->line(C->numNodes, (PLFLT*) C->haz,(PLFLT*) zCanopy);   //plot haz
     pls->mtex( "t", 3.0, 0.5, 0.5, "Non-dimenstional leaf area density" );
 
     delete pls; // close plot
@@ -250,47 +283,76 @@ void canopyFlow::plotWind(double inputSpeed, double inputHeight)
     z = NULL;
 }
 
+void canopyFlow::make_canopy(canopy::eCanopyType t)
+{
+    if(t == canopy::normal_distribution)
+        C = new canopy_normal_distribution;
+    else if(t == canopy::triangle)
+        C = new canopy_normal_distribution;
+    else if(t == canopy::Massman)
+        C = new canopy_normal_distribution;
+    else if(t == canopy::stepwise)
+        C = new canopy_normal_distribution;
+    else
+        throw std::logic_error("ERROR: Cannot determine canopy type in canopyFlow::make_canopy().\n");
+}
+
+void canopyFlow::make_canopy(canopy* X)
+{
+    delete C;
+    if(X->distributionType == canopy::normal_distribution)
+        C = new canopy_normal_distribution(*((canopy_normal_distribution*)X));
+    else if(X->distributionType == canopy::triangle)
+        C = new canopy_normal_distribution(*((canopy_normal_distribution*)X));
+    else if(X->distributionType == canopy::Massman)
+        C = new canopy_normal_distribution(*((canopy_normal_distribution*)X));
+    else if(X->distributionType == canopy::stepwise)
+        C = new canopy_normal_distribution(*((canopy_normal_distribution*)X));
+    else
+        throw std::logic_error("ERROR: Cannot determine canopy type in canopyFlow::make_canopy().\n");
+}
+
 void canopyFlow::computeWind()
 {
-    //C.initialize();
-    usuh = c1 - (c1 + K / log(C.z0gh)) * exp(-c3 *C.zetah);
+    //C->initialize();
+    usuh = c1 - (c1 + K / log(C->z0gh)) * exp(-c3 *C->zetah);
     double Csurf = 2.0 * usuh * usuh;
-    double nexp = C.zetah / Csurf;
+    double nexp = C->zetah / Csurf;
 
-    double* nzet = new double[C.numNodes];
-    uzc =  new double[C.numNodes];
-    uzcs =  new double[C.numNodes];
+    double* nzet = new double[C->numNodes];
+    uzc =  new double[C->numNodes];
+    uzcs =  new double[C->numNodes];
 
     double max_uzc = 0.0;
-    for(int i=0; i<C.numNodes; i++)
+    for(int i=0; i<C->numNodes; i++)
     {
-        nzet[i] = nexp * C.zetaz[i];
-        if(i*C.cellsize <= C.z0gh)
+        nzet[i] = nexp * C->zetaz[i];
+        if(i*C->cellsize <= C->z0gh)
             uzc[i] = 0.0;
         else
-            uzc[i] = log(i*C.cellsize / C.z0gh) * cosh(nzet[i]) / cosh(nexp);
+            uzc[i] = log(i*C->cellsize / C->z0gh) * cosh(nzet[i]) / cosh(nexp);
         if(uzc[i] > max_uzc)
             max_uzc = uzc[i];
     }
 
     delete nzet;
 
-    for(int i=0; i<C.numNodes; i++)
+    for(int i=0; i<C->numNodes; i++)
         uzc[i] /= max_uzc;
 
     uzcs[0] = 0.0;
-    for(int i=1;i<C.numNodes;i++) //calculate Reynolds stress using cumulative trapazoid
-        uzcs[i] = uzcs[i-1] + (C.hacpz[i-1]*uzc[i-1]*uzc[i-1] + C.hacpz[i]*uzc[i]*uzc[i]) * 0.5;
+    for(int i=1;i<C->numNodes;i++) //calculate Reynolds stress using cumulative trapazoid
+        uzcs[i] = uzcs[i-1] + (C->hacpz[i-1]*uzc[i-1]*uzc[i-1] + C->hacpz[i]*uzc[i]*uzc[i]) * 0.5;
 
-    for(int i=1;i<C.numNodes;i++)
-        uzcs[i] /= uzcs[C.numNodes-1];
+    for(int i=1;i<C->numNodes;i++)
+        uzcs[i] /= uzcs[C->numNodes-1];
 
     //compute lowest inflection point
-    int begin = (int) 2.0 * (C.z0gh/C.cellsize);
+    int begin = (int) 2.0 * (C->z0gh/C->cellsize);
     double difU;
     double difUOld = uzc[begin] - 2.0*uzc[begin-1] + uzc[begin-2];   //start by computing the value of the second derivative at node = begin-1
     int end;
-    for(int i=begin; i<C.numNodes-1; i++)
+    for(int i=begin; i<C->numNodes-1; i++)
     {
         difU = uzc[i+1] - 2.0*uzc[i] + uzc[i-1];
         if((difU*difUOld) < 0.0)   //if we've switched sign, break
@@ -307,7 +369,7 @@ void canopyFlow::computeWind()
 
      double inter1;
 
-    for(int i=0; i<C.numNodes; i++)   //integrate using extened Simpson's rule
+    for(int i=0; i<C->numNodes; i++)   //integrate using extened Simpson's rule
     {
         if(i%2 == 0)    //if even numbers
             inter1 += uzcs[i];
@@ -315,8 +377,8 @@ void canopyFlow::computeWind()
             inter1 += 2.0 * uzcs[i];
     }
 
-    inter1 = inter1 - 0.5 * (uzcs[0] + uzcs[C.numNodes-1]);
-    inter1 *= C.cellsize * 2.0/3.0;
+    inter1 = inter1 - 0.5 * (uzcs[0] + uzcs[C->numNodes-1]);
+    inter1 *= C->cellsize * 2.0/3.0;
     doh = 1.0 - inter1;
     z0oh = rough * inter1 * exp(-K/usuh);
     printf("doh = %lf\nz0oh = %lf\n", doh, z0oh);
@@ -324,21 +386,21 @@ void canopyFlow::computeWind()
 
 double canopyFlow::get_windspeed(double inputSpeed, double inputHeight, double desiredHeight)
 {
-    double uCanopyHeight = K * inputSpeed / (usuh * (log((inputHeight/C.canopyHeight - doh) / z0oh) + logRough));
-    //double uCanopyHeight = inputSpeed * log((1.0 - doh) / z0oh) / log((inputHeight/C.canopyHeight - doh) / z0oh);
-    if(desiredHeight <= C.z0g)  //if below ground roughness height
+    double uCanopyHeight = K * inputSpeed / (usuh * (log((inputHeight/C->canopyHeight - doh) / z0oh) + logRough));
+    //double uCanopyHeight = inputSpeed * log((1.0 - doh) / z0oh) / log((inputHeight/C->canopyHeight - doh) / z0oh);
+    if(desiredHeight <= C->z0g)  //if below ground roughness height
     {
         return 0.0;
-    }else if(desiredHeight < C.canopyHeight)  //below canopy
+    }else if(desiredHeight < C->canopyHeight)  //below canopy
     {
-        return uCanopyHeight * uzc[(int)(desiredHeight/(C.canopyHeight*C.cellsize))];
-    }else if(desiredHeight == C.canopyHeight)   //exactly at canopy height
+        return uCanopyHeight * uzc[(int)(desiredHeight/(C->canopyHeight*C->cellsize))];
+    }else if(desiredHeight == C->canopyHeight)   //exactly at canopy height
     {
         return uCanopyHeight;
     }else   //above canopy height
     {
-        //return uCanopyHeight * usuh * log((desiredHeight/C.canopyHeight - doh) / (z0oh + C.z0g/C.canopyHeight) / K;
-        return uCanopyHeight * usuh * (log((desiredHeight/C.canopyHeight - doh) / z0oh) + logRough) / K;
+        //return uCanopyHeight * usuh * log((desiredHeight/C->canopyHeight - doh) / (z0oh + C->z0g/C->canopyHeight) / K;
+        return uCanopyHeight * usuh * (log((desiredHeight/C->canopyHeight - doh) / z0oh) + logRough) / K;
     }
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,32 @@ int main() {
 
     canopyFlow wind;
 
-    //Set inputs
-    wind.C.leafAreaIndex = 1.0;
-    wind.C.canopyHeight = 10.0;                        //canopy height (m)
-    wind.C.z0g = 0.025;                      //ground roughness length (m)
-    wind.C.dragCoefAth = 0.2;
-    wind.C.heightMaxFoliageDist = 0.5;     //height of maximum foliage distribution for the normal distribution (m)
-    wind.C.standardDevFoliageDist = 0.25;   //standard deviation of foliage distribution for the normal distribution (m)
-    wind.C.numNodes = 10001;                 //number of cells to use for numerical integration
+    //-------Normal Distribution------------------------
+    //double heightMaxFoliageDist = 0.5;
+    //double standardDevFoliageDist = 0.25;
+    //wind.C = new canopy_normal_distribution(heightMaxFoliageDist, standardDevFoliageDist);
 
-    wind.C.initialize();
+    //-------Triangle Distribution---------------------
+    double A1 = 0.08;       //density of top
+    double Ax = 3;          //density at max point
+    double Ab = 0.18;       //density of bottom (trunk space)
+    double zmax = 0.5;      //height to Ax (0 < zmax < 1)
+    double zbot = 0.1;      //height to bottom of triangular part (0 < zbot < 1; zbot < zmax)
+    wind.C = new canopy_triangle_distribution(A1, Ax, Ab, zmax, zbot);
+
+    //Set inputs
+
+    wind.C->leafAreaIndex = 1.0;
+    wind.C->canopyHeight = 10.0;                        //canopy height (m)
+    wind.C->z0g = 0.025;                      //ground roughness length (m)
+    wind.C->dragCoefAth = 0.2;
+    wind.C->numNodes = 10001;                 //number of cells to use for numerical integration
+
+    wind.C->initialize();
     wind.computeWind();
 
-    //wind.C.plot();
-    wind.plotWind(15.0, wind.C.canopyHeight + 5.0);
+    //wind.C->plot();
+    wind.plotWind(15.0, wind.C->canopyHeight + 5.0);
 
     std::cout << "Done!" << std::endl;
     return 0;
