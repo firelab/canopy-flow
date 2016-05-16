02c7bdd068bc1f3ff8edda243af34bab78f50f01
Jason Forthofer <jforthofer@gmail.com>
2016-05-16 11:35:26 -0600

    adding another plot, WAF vs flame height for Massman's model

--- src/main_compare_albini.cpp
+++ src/main_compare_albini.cpp
@@ -11,7 +11,7 @@ int main() {
 
     //----Settings for flames below canopy top case----
     double canopyHeight = 10.0;                 //canopy height (m)
-    //double canopyCover = 0.7;                   //canopy cover [0..1]
+    double canopyCover = 0.7;                   //canopy cover [0..1]
     double crownRatio = 0.5;                    //crown ratio [0..1]
     double dragCoef = 0.2;                      //drag coefficient (Albini used 1.0, Massman likes 0.2)
     double fuelBedDepth = 0.5 / 3.28084;                  //fuel bed depth (m)
@@ -28,6 +28,12 @@ int main() {
     double* WAFarrayAlbiniAbove = new double[plotNodes];
     double* WAFarrayMassmanAbove = new double[plotNodes];
 
+    double* flameHeightBelow = new double[plotNodes];
+    double* flameHeightAbove = new double[plotNodes];
+    double* WAFBelowIntegral = new double[plotNodes];
+    double* WAFBelowMidFlame = new double[plotNodes];
+    double* WAFAboveIntegral = new double[plotNodes];
+
     WindAjustmentFactor albini;
     canopyFlow massman;
 
@@ -40,6 +46,8 @@ int main() {
     massman.computeWind();
 
     double canopyCoverStepSize = 1.0 / (plotNodes-1);
+    double flameHeightStepSizeAbove = (inputHeight-canopyHeight) / (plotNodes);
+    double flameHeightStepSizeBelow = canopyHeight / (plotNodes-1);
 
     //build plot arrays
     for(int i=0; i<plotNodes; i++)
@@ -49,6 +57,15 @@ int main() {
         massman.computeWind();
         WAFarrayMassmanBelow[i] = massman.get_windAdjustmentFactorShelteredMidFlame(inputHeight, midFlameHeight);
         WAFarrayAlbiniBelow[i] = albini.calculateWindAdjustmentFactor(canopyCoverArray[i], canopyHeight*3.28084, crownRatio, fuelBedDepth*3.28084);  //arguments converted to feet here
+
+        //reset canopy cover
+        massman.C->leafAreaIndex = canopyHeight * crownRatio * canopyCover * betaSigma / (6.0 * pi);
+        massman.computeWind();
+        flameHeightAbove[i] = flameHeightStepSizeAbove + canopyHeight + i * flameHeightStepSizeAbove;   //Don't include the canopy height, start above that
+        flameHeightBelow[i] = i * flameHeightStepSizeBelow;
+        WAFBelowIntegral[i] = massman.get_windAdjustmentFactorShelteredIntegral(inputHeight, flameHeightBelow[i]);
+        WAFBelowMidFlame[i] = massman.get_windAdjustmentFactorShelteredMidFlame(inputHeight, flameHeightBelow[i]/2.0);
+        WAFAboveIntegral[i] = massman.get_windAdjustmentFactorUnshelteredIntegral(inputHeight, flameHeightAbove[i]);
     }
 
     //-------Uniform Distribution, Above Canopy Flames-----------------------
@@ -191,6 +208,109 @@ int main() {
 
     delete pls; // close plot
 
+
+
+
+
+
+    //----Make new plot--------------------------------
+    xmin = flameHeightBelow[0], ymin = 0.0, xmax = flameHeightAbove[plotNodes-1], ymax = 1.0;
+
+    just=0, axis=0;
+    //plstream *pls;
+
+    // plplot initialization
+
+    pls = new plstream();  // declare plplot object
+
+    plsdev("svg"); //cairo uses the same color scheme as on screen - black on
+    //red on black default
+    plsfnam("massman_vs_flameHeight.svg");// sets the names of the output file
+
+    plscolbg(255,255,255);  //change background color
+
+    pls->init();           // start plplot object
+    plscol0(1, 0, 0, 0);        //change the color pallet0 color #1 to be black (0,0,0)
+    plscol0(2, 255, 0, 0);      //change the color pallet0 color #2 to be red (255,0,0)
+    plscol0(3, 255, 255, 255);  //change the color pallet0 color #3 to be white (255,255,255)
+    plcol0(1);              //now change our font color to be the color #1
+
+    pls->adv( 0 );
+    pls->vpor( 0.15, 0.85, 0.1, 0.8 );
+    pls->wind( xmin, xmax, ymin, ymax);
+    pls->box( "bcnst", 0.0, 0, "bcnst", 0.0, 0 );
+
+    pls->line(plotNodes, (PLFLT*) flameHeightBelow,(PLFLT*) WAFBelowIntegral);   //plot below integral
+    pls->line(plotNodes, (PLFLT*) flameHeightAbove,(PLFLT*) WAFAboveIntegral);   //plot above integral
+    plcol0(2);              //now change our font color to be the color #2
+    pls->line(plotNodes, (PLFLT*) flameHeightBelow,(PLFLT*) WAFBelowMidFlame);   //plot below midflame
+    plcol0(1);              //now change our font color to be the color #1
+
+    pls->schr(0, 1.6);  //change font size
+    pls->mtex( "t", 4.0, 0.5, 0.5, "Massman flame height comparison" );
+    pls->schr(0, 1.0);  //change font size
+    pls->mtex( "b", 3.0, 0.5, 0.5, "Flame Height (m)" );
+    pls->mtex( "l", 3.0, 0.5, 0.5, "WAF" );
+
+    // Draw a legend
+   nlegend = 2;
+//    const char   *text[4], *symbols[4];
+//    PLINT        opt_array[4];
+//    PLINT        text_colors[4];
+//    PLINT        line_colors[4];
+//    PLINT        line_styles[4];
+//    PLFLT        line_widths[4];
+//    PLINT        symbol_numbers[4], symbol_colors[4];
+//    PLFLT        symbol_scales[4];
+//    PLFLT        legend_width, legend_height;
+
+    // First legend entry.
+    opt_array[0]   = PL_LEGEND_LINE;
+    text_colors[0] = 1;
+    text[0]        = "Integral method";
+    line_colors[0] = 1;
+    line_styles[0] = 1;
+    line_widths[0] = 1.;
+    symbols[0] = "";
+
+    // Second legend entry.
+    opt_array[1]      = PL_LEGEND_LINE;
+    text_colors[1]    = 2;
+    text[1]           = "Midflame method";
+    line_colors[1]    = 2;
+    line_styles[1]    = 1;
+    line_widths[1]    = 1.;
+    //symbol_colors[1]  = 3;
+    //symbol_scales[1]  = 1.;
+    //symbol_numbers[1] = 4;
+    symbols[1]        = "";
+    // from the above opt_arrays we can completely ignore everything
+    // to do with boxes.
+
+    plscol0a( 15, 32, 32, 32, 0.70 );
+
+    pls->legend(&legend_width, &legend_height,
+                PL_LEGEND_BACKGROUND | PL_LEGEND_BOUNDING_BOX, 0,
+                0.44, 0.03, 0.1, 3,
+                1, 1, 1, 1,
+                nlegend, opt_array,
+                2.0, 1.0, 2.0,
+                1., text_colors, (const char **) text,
+                NULL, NULL, NULL, NULL,
+                line_colors, line_styles, line_widths,
+                symbol_colors, symbol_scales, symbol_numbers, (const char **) symbols );
+
+    delete pls; // close plot
+
+
+
+
+
+
+
+
+
+
     delete canopyCoverArray;
     canopyCoverArray = NULL;
     delete WAFarrayAlbiniBelow;
@@ -202,6 +322,16 @@ int main() {
     delete WAFarrayMassmanAbove;
     WAFarrayMassmanAbove = NULL;
 
+    delete flameHeightBelow;
+    flameHeightBelow = NULL;
+    delete flameHeightAbove;
+    flameHeightAbove = NULL;
+    delete WAFBelowIntegral;
+    WAFBelowIntegral = NULL;
+    delete WAFBelowMidFlame;
+    WAFBelowMidFlame = NULL;
+    delete WAFAboveIntegral;
+    WAFAboveIntegral = NULL;
 
     std::cout << "Done!" << std::endl;
     return 0;
